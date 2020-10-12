0662a48becc08443566d2e737f3506092ea667ee
guangze.song <songguangze@fotoable.com>
2020-10-12 11:41:55 +0800

    leetcode 39，回溯算法

--- leecode-cn/leetcode39-CombinationSum/CombinationSum.cpp
+++ leecode-cn/leetcode39-CombinationSum/CombinationSum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ class Solution
 public:
 	vector<vector<int>> combinationSum(vector<int>& candidates, int target);
 
-	void backTrace(vector<int>& candidates, int target, vector<int>& currentCombination, vector<vector<int>>& vecResult);
+	void backTrace(const vector<int> candidates, int target, vector<int>& currentCombination, vector<vector<int>>& vecResult);
 
 	bool addCombinationToResult2(vector<int> vecCombination, vector<vector<int>>& vecResult);
 };
@@ -17,18 +18,71 @@ public:
 int main()
 {
 	Solution s;
-	vector<int> vecCandidates = { 2,3,6,7 };
-	int target = 9;
+	vector<int> vecCandidates = { 2,3,5 };
+	int target = 8;
+	vector<vector<int>> candidateResult = s.combinationSum(vecCandidates, target);
 
+	cout << candidateResult.size();
 	return 0;
 }
 
-bool Solution::combinationSum(vector<int>& candidates, int target)
+vector<vector<int>>  Solution::combinationSum(vector<int>& candidates, int target)
 {
+	vector<vector<int>> vecCombinationResult;
+	vector<int> vecCurrentCombination;
+	sort(candidates.begin(), candidates.end());
 
+	backTrace(candidates, target, vecCurrentCombination, vecCombinationResult);
+
+	return vecCombinationResult;
 }
 
-void Solution::backTrace(vector<int>& candidates, int target, vector<int>& currentCombination, vector<vector<int>>& vecResult)
+void Solution::backTrace(const vector<int> candidates, int target, vector<int>& currentCombination, vector<vector<int>>& vecResult)
 {
+	if (target == 0)
+	{
+		addCombinationToResult2(currentCombination, vecResult);
+		return;
+	}
 
+	if (target < 0)
+		return;
+
+	for (int i = 0; i < candidates.size(); ++i)
+	{
+		int candidateElement = candidates[i];
+		currentCombination.push_back(candidateElement);
+		backTrace(candidates, target - candidateElement, currentCombination, vecResult);
+		currentCombination.pop_back();
+	}
+}
+
+
+bool Solution::addCombinationToResult2(vector<int> vecCombination, vector<vector<int>>& vecResult)
+{
+	bool isExistResult = false;
+	sort(vecCombination.begin(), vecCombination.end());
+	for (vector<int> existResult : vecResult)
+	{
+		if (existResult.size() == vecCombination.size())
+		{
+			isExistResult = true;
+			for (int i = 0; i < existResult.size(); ++i)
+				isExistResult = isExistResult && existResult[i] == vecCombination[i];
+
+			if (isExistResult)
+				break;
+		}
+	}
+
+	if (!isExistResult)
+	{
+		vector<int>* pResult = new vector<int>();
+		for (int combinationElement : vecCombination)
+			pResult->push_back(combinationElement);
+
+		vecResult.push_back(*pResult);
+	}
+
+	return !isExistResult;
 }
